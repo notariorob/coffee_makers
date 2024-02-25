348e507306a51b85bc559fab3a9475cf324620d8
Roberto Notario <notariorob@gmail.com>
2024-02-25 22:12:54 +0000

    refactor Product class constructor and add new properties

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,15 +1,20 @@
 class Product {
+  final int id;
   final String name;
+  final String description;
   final double price;
   final String image;
-  final List<String> categories;
+  final String category;
 
-  String get imageUrl => 'https://blablabla/$image';
+  String get imageUrl =>
+      'https://firtman.github.io/coffeemasters/api/images/$image';
 
-  Product(
-      {required this.name,
-      required this.price,
-      required this.image,
-      List<String>? categories})
-      : categories = categories ?? [];
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.category,
+  });
 }
