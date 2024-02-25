d158934e4f36a199456b8e15b71a469ad3dd0d70
Roberto Notario <notariorob@gmail.com>
2024-02-25 22:26:39 +0000

    make quantity non-final & include a fromJson constructor

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -2,7 +2,11 @@ import 'package:coffee_makers/models/product.dart';
 
 class CartItem {
   final Product product;
-  final int quantity;
+  int quantity;
 
   CartItem({required this.product, required this.quantity});
+
+  CartItem.fromJson(Map<String, dynamic> json)
+      : product = Product.fromJson(json['product']),
+        quantity = json['quantity'];
 }
